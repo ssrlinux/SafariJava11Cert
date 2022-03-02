f87cd430a13bbf1b141cb519362d5f6753baeea4
simon <simon@dancingcloudservices.com>
2022-03-02 11:13:08 -0700

    Day 1 Break 1

---
+++ src/main/java/usevar/Q1.java
@@ -0,0 +1,29 @@
+package usevar;
+
+//class X {
+//  int x = 99;
+//}
+
+public class Q1 {
+  public static void useArray(int [] ia) {}
+//
+//  void doStuff(var x) { }
+//
+//  void doStuff() {
+//    var x;
+//    x = 100;
+//  }
+//
+//  void doStuff() {
+//    var x = 99;
+////    x = 3L;
+//  }
+//
+//  void doStuff() {
+////    var x = { 1, 2, 3 };
+//    var x = new int[]{ 1, 2, 3 };
+////    int [] ia = {1, 2, 3}; // type inferencing!!
+//    int [] ia = new int[]{1, 2, 3};
+//    useArray(new int[]{1, 2, 3});
+//  }
+}
